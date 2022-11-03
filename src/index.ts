bfb03ebaa81ddcf5eaa3032a405f809e2e586b70
Aguiar <lucas.nune@outlook.com.br>
2022-11-03 18:34:00 -0300

    Base de post/get cadastro

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,18 @@ import express, {Request, Response, NextFunction} from 'express';
 const app = express(); 
 
 app.get('/status', (req: Request, res:Response, next:NextFunction) => {
+        res.status(200).send({foo:'bar 10'})
+});
 
+app.get('/cadastro', (req: Request, res:Response, next:NextFunction) => {
+    res.status(200).send({
+        nome:'',
+        email:'',
+        cep:'',
+        telefone:'',
+        cidade:''})
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!')
 });
